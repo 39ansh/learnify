f0dd18088857a332874900cd75188d2de32a9381
39ansh <141595198+39ansh@users.noreply.github.com>
2024-07-25 00:32:11 -0600

    login page

--- components/Login.js
+++ components/Login.js
@@ -1,10 +1,23 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, Image, StyleSheet, Text} from 'react-native';
 
 export default function Login() {
   return (
-    <View>
-      <Image source={require}
+    <View style={styles.container}>
+      <Image source={require('./Images/login.png')} style={styles.image} />
+      <Text>Welcome to learnify</Text>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: 100,
+    height: 100,
+  },
+});
